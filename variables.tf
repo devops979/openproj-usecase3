ac34595cec4585125b1f5203c282c49d07a26041
anil357208 <tbs.anil777@gmail.com>
2025-06-03 15:11:06 +0000

    removed nginx installation

--- variables.tf
+++ variables.tf
@@ -53,7 +53,7 @@ variable "ami_id" {
 variable "instance_type" {
   description = "Instance type for the EC2 instances"
   type        = string
-  default     = "t2.micro"
+  default     = "t2.medium"
 }
 
 
