12d42abb406e749c74e913d05b2fb1203b1a3f9c
devops979 <devops979@gmail.com>
2025-06-03 11:57:00 +0000

    public subnet variable issue resolved

--- variables.tf
+++ variables.tf
@@ -10,11 +10,19 @@ variable "cidr_block" {
   default     = "10.0.0.0/16"
 }
 
+#variable "public_subnet_cidrs" {
+#  description = "List of CIDR blocks for public subnets"
+#  type        = list(string)
+#  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+#}
+
+
 variable "public_subnet_cidrs" {
   description = "List of CIDR blocks for public subnets"
-  type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+  type        = string
+  default     = "10.0.1.0/24"
 }
+
 
 variable "private_subnet_cidrs" {
   description = "List of CIDR blocks for private subnets"
